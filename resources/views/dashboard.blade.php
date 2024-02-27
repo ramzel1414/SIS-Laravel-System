2ab0d31362152774b5fc8b54069435857ed8a0ed
jeffreysedoro <jeffreysedoro5@gmail.com>
2024-02-27 19:16:15 +0800

    Created a folder of [student,subject,grade] and added a laravel breeze for login/logout auth

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,82 +1,17 @@
-<!DOCTYPE html>
-<!--
-Template Name: NobleUI - HTML Bootstrap 5 Admin Dashboard Template
-Author: NobleUI
-Website: https://www.nobleui.com
-Portfolio: https://themeforest.net/user/nobleui/portfolio
-Contact: nobleui123@gmail.com
-Purchase: https://1.envato.market/nobleui_admin
-License: For each use you must have a valid license purchased only from above link in order to legally use the theme for your project.
--->
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <meta name="description" content="Responsive HTML Admin Dashboard Template based on Bootstrap 5">
-	<meta name="author" content="NobleUI">
-	<meta name="keywords" content="nobleui, bootstrap, bootstrap 5, bootstrap5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
 
-	<title>Student Information System</title>
-
-  <!-- Fonts -->
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
-  <!-- End fonts -->
-
-	<!-- core:css -->
-	<link rel="stylesheet" href="../assets/vendors/core/core.css">
-	<!-- endinject -->
-
-	<!-- Plugin css for this page -->
-	<link rel="stylesheet" href="../assets/vendors/flatpickr/flatpickr.min.css">
-	<!-- End plugin css for this page -->
-
-	<!-- inject:css -->
-	<link rel="stylesheet" href="../assets/fonts/feather-font/css/iconfont.css">
-	<link rel="stylesheet" href="../assets/vendors/flag-icon-css/css/flag-icon.min.css">
-	<!-- endinject -->
-
-  <!-- Layout styles -->  
-	<link rel="stylesheet" href="../assets/css/demo2/style.css">
-  <!-- End layout styles -->
-
-  <link rel="shortcut icon" href="../assets/images/favicon.png" />
-</head>
-<body>
-
-        @include('custom-layouts.sidebar');
-		<!-- partial -->
-	
-		<div class="page-wrapper">
-					
-            @include('custom-layouts.header');
-            
-            @yield('content');
-
-            @include('custom-layouts.footer')
-		
-		</div>
-	</div>
-
-	<!-- core:js -->
-	<script src="../assets/vendors/core/core.js"></script>
-	<!-- endinject -->
-
-	<!-- Plugin js for this page -->
-  <script src="../assets/vendors/flatpickr/flatpickr.min.js"></script>
-  <script src="../assets/vendors/apexcharts/apexcharts.min.js"></script>
-	<!-- End plugin js for this page -->
-
-	<!-- inject:js -->
-	<script src="../assets/vendors/feather-icons/feather.min.js"></script>
-	<script src="../assets/js/template.js"></script>
-	<!-- endinject -->
-
-	<!-- Custom js for this page -->
-  <script src="../assets/js/dashboard-dark.js"></script>
-	<!-- End custom js for this page -->
-
-</body>
-</html>    
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\StudentController;
-use App\Http\Controllers\TeacherController;
-use App\Http\Controllers\GradeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,51 +18,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/logout', function () {
-    Auth::logout(); // Log the user out
-    return redirect('/'); // Redirect to the welcome page or any other page
-})->name('logout');
-
-
 Route::get('/dashboard', function () {
-    return view('index');
+    return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
-
-// Student-related routes group
-Route::prefix('students')->group(function () {
-    // Route for listing students
-    Route::get('/', [StudentController::class, 'index'])->name('students.index');
-
-    // Route for viewing a specific student
-    Route::get('/{student}', [StudentController::class, 'show'])->name('students.show');
-
-    // Add more student-related routes as needed
-});
-
-// Teacher-related routes group
-Route::prefix('teachers')->group(function () {
-    // Route for listing students
-    Route::get('/', [TeacherController::class, 'index'])->name('teachers.index');
-
-    // Route for viewing a specific student
-    Route::get('/{teachers}', [TeacherController::class, 'show'])->name('teachers.show');
-
-    // Add more student-related routes as needed
-});
-
-// Teacher-related routes group
-Route::prefix('grades')->group(function () {
-    // Route for listing students
-    Route::get('/', [GradeController::class, 'index'])->name('grades.index');
-
-    // Route for viewing a specific student
-    Route::get('/{grades}', [GradeController::class, 'show'])->name('grades.show');
-
-    // Add more student-related routes as needed
-});
-
-
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
