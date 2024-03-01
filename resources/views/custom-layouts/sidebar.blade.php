9f11a3b05563eba87fac22853e5234e85b1a0e40
ramzel1414 <ramonpaulocaumban@gmail.com>
2024-03-02 03:58:40 +0800

    update welcome page

--- resources/views/custom-layouts/sidebar.blade.php
+++ resources/views/custom-layouts/sidebar.blade.php
@@ -4,7 +4,7 @@
     <nav class="sidebar">
   <div class="sidebar-header">
     <a href="#" class="sidebar-brand">
-      Noble<span>UI</span>
+      SIS-<span>UI</span>
     </a>
     <div class="sidebar-toggler not-active">
       <span></span>
